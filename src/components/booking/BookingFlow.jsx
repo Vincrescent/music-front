c1cfd09735671020aed79a03ffd1eaca64d2713d
shed <121990815+ShadeeSkiee@users.noreply.github.com>
2026-07-23 23:07:58 +0700

    Fix_missing_useEffect_import_in_BookingFlow_and_enforce_light_mode_white_backgrounds

--- src/components/booking/BookingFlow.jsx
+++ src/components/booking/BookingFlow.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import api from "../../utils/axiosConfig";
 import { X, CheckCircle, PartyPopper, Lock } from "lucide-react";
 import { studios } from "../../data/booking";
